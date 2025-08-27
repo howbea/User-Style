69fa87405c4610cbc6908762886e02933ccb1ed3
howbea <150657314+howbea@users.noreply.github.com>
2025-08-27 09:41:14 -0400

    Add files via upload

--- signalshell@github.com.howbea/prefs.js
+++ signalshell@github.com.howbea/prefs.js
@@ -1,72 +1,168 @@
-/* pref.js
-*
-* this js is based on 'panel scroll - sun.wxg@gmail.com' extension.
-* https://github.com/sunwxg/gnome-shell-extension-panelScroll
-*
-*/
+// SPDX-FileCopyrightText: 2020 Florian Müllner <fmuellner@gnome.org>
+// SPDX-FileCopyrightText: 2024 Yoshiki Takahashi <m32rncsz@gmail.com>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-const { Gtk, Gio } = imports.gi;
+// -*- mode: js2; indent-tabs-mode: nil; js2-basic-offset: 4 -*-
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
+// we use async/await here to not block the mainloop, not to parallelize
+/* eslint-disable no-await-in-loop */
 
-const KEY_DEFDARK = 'default-dark';
+import Adw from 'gi://Adw';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
 
-function init() {
-}
+import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
+import {getThemeDirs, getModeThemeDirs} from './util.js';
 
-function buildPrefsWidget() {
-    let settings = ExtensionUtils.getSettings();
+Gio._promisify(Gio.File.prototype, 'enumerate_children_async');
+Gio._promisify(Gio.File.prototype, 'query_info_async');
+Gio._promisify(Gio.FileEnumerator.prototype, 'next_files_async');
 
-    let widget = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        margin_top: 10,
-        margin_bottom: 10,
-        margin_start: 10,
-        margin_end: 10,
-    });
-
-    let vbox = new Gtk.Box({
-        orientation: Gtk.Orientation.VERTICAL,
-        margin_top: 10
-    });
-    vbox.set_size_request(550, 350);
-let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_top: 5});
-    vbox.append(addItemSwitch("Change default variant", KEY_DEFDARK, settings));
-
-    widget.append(vbox); widget.append(hbox);
-
-    return widget;
-}
-
-function addItemSwitch(string, key, gsettings) {
-        let hbox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, margin_top: 20});
-        let info = new Gtk.Label({xalign: 0, hexpand: true});
-        info.set_markup(string);
-        hbox.append(info);
-
-        let button = new Gtk.Switch({ active: gsettings.get_boolean(key) });
-        button.connect('notify::active', (button) => { gsettings.set_boolean(key, button.active); });
-        hbox.append(button);
-        return hbox;
+class UserThemeGroup extends Adw.PreferencesGroup {
+    static {
+        GObject.registerClass(this);
     }
-    
-function addText(key, placeholder_text, gsettings) {
-        let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_top: 5});
-        
-        let info = new Gtk.Label({xalign: 0, hexpand: true});        
-        //info.set_markup(string);
-        
-        let settingentry = new Gtk.Entry({hexpand: true, margin_start: 20});
-        settingentry.set_placeholder_text(placeholder_text);
-        settingentry.set_text(gsettings.get_string(key));
-        settingentry.connect('changed', (entry) => {
-            gsettings.set_string(key, entry.get_text());
+
+    constructor(settings) {
+        super({title: 'Themes'});
+
+        this._actionGroup = new Gio.SimpleActionGroup();
+        this.insert_action_group('theme', this._actionGroup);
+
+        this._settings = settings;
+        this._actionGroup.add_action(
+            this._settings.create_action('name'));
+
+        this.connect('destroy', () => (this._settings = null));
+
+        this._rows = new Map();
+        this._addTheme(''); // default
+
+        this._collectThemes();
+    }
+
+    async _collectThemes() {
+        for (const dirName of getThemeDirs()) {
+            const dir = Gio.File.new_for_path(dirName);
+            for (const name of await this._enumerateDir(dir)) {
+                if (this._rows.has(name))
+                    continue;
+
+                const file = dir.resolve_relative_path(
+                    `${name}/gnome-shell/gnome-shell-dark.css`);
+                try {
+                    await file.query_info_async(
+                        Gio.FILE_ATTRIBUTE_STANDARD_NAME,
+                        Gio.FileQueryInfoFlags.NONE,
+                        GLib.PRIORITY_DEFAULT, null);
+                    this._addTheme(name);
+                } catch (e) {
+                    if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.NOT_FOUND))
+                        logError(e);
+                }
+            }
+        }
+
+        for (var dirName of getModeThemeDirs()) {
+            var dir = Gio.File.new_for_path(dirName);
+            for (var filename of await this._enumerateDir(dir)) {
+                if (!filename.endsWith('-dark.css'))
+                    continue;
+
+                var name = filename.slice(0, -9);
+                if (!this._rows.has(name))
+                    this._addTheme(name);
+            }
+        }
+    }
+
+    _addTheme(name) {
+        const row = new ThemeRow(name);
+        this._rows.set(name, row);
+
+        this.add(row);
+    }
+
+    async _enumerateDir(dir) {
+        const fileInfos = [];
+        let fileEnum;
+
+        try {
+            fileEnum = await dir.enumerate_children_async(
+                Gio.FILE_ATTRIBUTE_STANDARD_NAME,
+                Gio.FileQueryInfoFlags.NONE,
+                GLib.PRIORITY_DEFAULT, null);
+        } catch (e) {
+            if (!e.matches(Gio.IOErrorEnum, Gio.IOErrorEnum.NOT_FOUND))
+                logError(e);
+            return [];
+        }
+
+        let infos;
+        do {
+            infos = await fileEnum.next_files_async(100,
+                GLib.PRIORITY_DEFAULT, null);
+            fileInfos.push(...infos);
+        } while (infos.length > 0);
+
+        return fileInfos.map(info => info.get_name());
+    }
+}
+
+class ThemeRow extends Adw.ActionRow {
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor(name) {
+        const check = new Gtk.CheckButton({
+            action_name: 'theme.name',
+            action_target: new GLib.Variant('s', name),
         });
 
-        hbox.append(info);
-        hbox.append(settingentry);
-        return hbox;
+        super({
+            title: name || 'Default',
+            activatable_widget: check,
+        });
+        this.add_prefix(check);
     }
+}
 
+const OptionsGroup = GObject.registerClass(
+class OptionsGroup extends Adw.PreferencesGroup {
+    _init(settings) {
+        super._init({ title: 'Options' });
+
+        //settings = this.ExtensionUtils.getSettings();
+        
+        const dlSwitch = new Gtk.Switch({
+            valign: Gtk.Align.CENTER,
+        });
+        settings.bind('default-dark',
+            dlSwitch, 'active',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        const row = new Adw.ActionRow({
+            title: 'Default Dark',
+            activatable_widget: dlSwitch,
+        });
+        row.add_suffix(dlSwitch);
+        this.add(row);
+    }
+});
+
+export default class UserThemePrefs extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+    const settings = this.getSettings();
+    const page = new Adw.PreferencesPage();
+    const userthemegroup = new UserThemeGroup(settings);
+    const optionsgroup = new OptionsGroup(settings);
+    page.add(userthemegroup);
+    page.add(optionsgroup);
+    window.add(page);
+    }
+}
